db6ecc32ea7fc0f231851e97c27b9cc8ff868442
aathi1711 <aathivdr2004@gmail.com>
2025-05-05 12:20:16 +0530

    done

--- src/todos.jsx
+++ src/todos.jsx
@@ -7,10 +7,10 @@ const Todos = ({todos,setTodos}) => {
         <h1 className='text-2xl font-semibold text-gray-200 p-1'>Your Todos :</h1>
         <div className='flex gap-2 justify-center flex-wrap'>
           { 
-          todos.length>0 ?
+          
           todos.map((todo,index)=>{
             return <Card todo={todo} key={index} index={index} todos = {todos} setTodos={setTodos}/>
-          }) : <p className='p-1 mt-5 text-gray-400'>Your Todo will be shown here.</p>}
+          })}
         </div>
     </div>
   )
